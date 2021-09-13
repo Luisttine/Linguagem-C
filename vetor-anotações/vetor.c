f609709e53ed7640327c665b4ddee448ae77c3a3
Luis Souza <luisttine@gmail.com>
2021-09-13 13:00:09 -0300

    vetores

---
+++ vetor-anotações/vetor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+void main () {
+
+	int i, vetor[10];
+
+	printf("Entre com 10 numeros:\n");
+
+	for (i=0; i<10; i++)
+		scanf("%d", &vetor[i]);
+	for (i=0; i<10; i++)
+		printf("%d", vetor[i]);
+                                                                                                                                                               
+}
